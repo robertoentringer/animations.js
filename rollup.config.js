e7dba82d11a920b3983fe12e462dd37c1252531e
Roberto Entringer <r.entringer@gmail.com>
2019-08-27 21:42:57 +0200

    use rollup instead of parcel and clear not necessary packs

---
+++ rollup.config.js
@@ -0,0 +1,11 @@
+import { terser } from "rollup-plugin-terser"
+
+export default (async () => ({
+  input: "src/animations.js",
+  output: {
+    file: "dist/animations.js",
+    format: "umd",
+    name: "animations"
+  },
+  plugins: [terser()]
+}))()
